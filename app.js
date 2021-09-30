ef5800c7c8e41dc7c245f0d49e455563136e2515
Anshul Bheniye <81631443+anshul-bheniye@users.noreply.github.com>
2021-09-30 22:56:52 +0530

    completed

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-alert('I am alive');
+let initialPrice = document.querySelector('.initial-price');
+let quantityStocks = document.querySelector('.quantity-stocks');
+let currentPrice = document.querySelector('.current-price');
+let btn = document.querySelector('#calculate');
+let output = document.querySelector('.output');
+
+function calculateStock(){
+    let ip = initialPrice.value;
+    let curr = currentPrice.value;
+    let qty = quantityStocks.value;
+
+    if(ip > curr){
+        var loss = (ip - curr) * qty;
+        var lossPercentage = (loss / ip) * 100;
+        output.innerText = `Hey the loss is ${loss} and percentage is ${lossPercentage}%`;
+    } else if (curr > ip){
+        var profit = (curr - ip)  * qty;
+        var profitPercentage = (profit / ip) * 100;
+        output.innerText =  `Hey the profit is ${profit} and percentage is ${profitPercentage}%`;
+    } else {
+        output.innerText = 'Patience is the key. Stocks do not immediately goes up';
+    }
+    
+}
+
+btn.addEventListener('click', calculateStock);
